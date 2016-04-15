d9d0a7d0c0cc0c144e42a073c50a9bd1421de99f
Linda Chang <linda_chang@brown.edu>
2016-04-15 17:22:13 -0400

    trying to connect input field

--- server.js
+++ server.js
@@ -26,48 +26,46 @@ var req = new XMLHttpRequest();
 // 	req.send(null);
 // }
 
-app.post('/:inputurl', function(request, response){
-	console.log(request.params.inputurl);
+app.post('/data/:inputurl', function(request, response) {
 	url = 'https://api.github.com/repos/Teradata/' + request.params.inputurl + '/pulls';
-	console.log(url, response);
-	// req.open('GET', url, true);
+	console.log(url);
 
-	// var token = 'f73a348b548c80c4675caad03698cc47fc74703f'; 	// PUT YOUR PERSONAL TOKEN HERE!!!
-
-	// req.setRequestHeader('Authorization', 'token ' + token);
-	// req.addEventListener('load', function(e){
-	// 	if (req.status == 200) {
-	// 		var data = JSON.parse(req.responseText);
-	// 		response.json(data);
-	// 	}
-	// }, false);
-
-	// req.send(null);
-	//console.log("server: " + url);
-});
-
-app.get('/', function(request, response){
-	response.render('home.html');
-});
-
-app.get('/data', function(request, response) {
-	//console.log('post' + request.params.data);
-	var url = 'https://api.github.com/repos/Teradata/presto/pulls';
-	//getDataInput(url, res);
 	req.open('GET', url, true);
 
-	var token = // PUT YOUR PERSONAL TOKEN HERE!!!
+	var token = 'b971c66a6876555d225ac25abed8dc177af18c61'; // PUT YOUR PERSONAL TOKEN HERE!!!
 
 	req.setRequestHeader('Authorization', 'token ' + token);
 	req.addEventListener('load', function(e){
 		if (req.status == 200) {
 			var data = JSON.parse(req.responseText);
+			
 			response.json(data);
 		}
 	}, false);
 
 	req.send(null); 
 });
+
+app.get('/', function(request, response){
+	response.render('home.html');
+});
+
+// app.get('/data', function(request, response) {
+// 	var url = 'https://api.github.com/repos/Teradata/presto/pulls';
+// 	req.open('GET', url, true);
+
+// 	var token = 'b971c66a6876555d225ac25abed8dc177af18c61'; // PUT YOUR PERSONAL TOKEN HERE!!!
+
+// 	req.setRequestHeader('Authorization', 'token ' + token);
+// 	req.addEventListener('load', function(e){
+// 		if (req.status == 200) {
+// 			var data = JSON.parse(req.responseText);
+// 			response.json(data);
+// 		}
+// 	}, false);
+
+// 	req.send(null); 
+// });
 
 // app.listen(process.env.PORT, function(){
 //     console.log('- Server listening on port 8080');
