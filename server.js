0db05e905e698d2ac4d0e1f2a1851cd1852df434
Linda Chang <linda_chang@brown.edu>
2016-04-20 17:00:52 -0400

    should be newest version now

--- server.js
+++ server.js
@@ -26,46 +26,13 @@ var req = new XMLHttpRequest();
 // 	req.send(null);
 // }
 
-app.post('/:inputurl', function(request, response){
-	console.log(request.params.inputurl);
+app.post('/data/:inputurl', function(request, response) {
 	url = 'https://api.github.com/repos/Teradata/' + request.params.inputurl + '/pulls';
-	console.log(url, response);
-	// req.open('GET', url, true);
-
-	// var token = 'f73a348b548c80c4675caad03698cc47fc74703f'; 	// PUT YOUR PERSONAL TOKEN HERE!!!
-
-	// req.setRequestHeader('Authorization', 'token ' + token);
-	// req.addEventListener('load', function(e){
-	// 	if (req.status == 200) {
-	// 		var data = JSON.parse(req.responseText);
-	// 		response.json(data);
-	// 	}
-	// }, false);
-
-	// req.send(null);
-	//console.log("server: " + url);
-});
-
-app.get('/', function(request, response){
-	response.render('home.html');
-});
-
-app.get('/data', function(request, response) {
-
-	//console.log('post' + request.params.data);
-	var url = 'https://api.github.com/repos/Teradata/presto/pulls';
-	//getDataInput(url, res);
 	req.open('GET', url, true);
 
-	var token = '3d5943bd7a782110eca24500e3e01b2510eadf3a';// PUT YOUR PERSONAL TOKEN HERE!!!
+	var token = '3d5943bd7a782110eca24500e3e01b2510eadf3a'; // PUT YOUR PERSONAL TOKEN HERE!!!
 
 	req.setRequestHeader('Authorization', 'token ' + token);
-	//req.setRequestHeader('state', 'closed');
-	// req.setRequestHeader('per_page', 100);
-	// req.params.state = "closed";
-	console.log(req);
-
-
 	req.addEventListener('load', function(e){
 		if (req.status == 200) {
 			var data = JSON.parse(req.responseText);
@@ -73,8 +40,46 @@ app.get('/data', function(request, response) {
 		}
 	}, false);
 
-	// req.send(null); 
-	req.send();
+	req.send(null); 
+});
+
+app.get('/', function(request, response){
+	response.render('home.html');
+});
+
+// app.get('/data', function(request, response) {
+
+// 	//console.log('post' + request.params.data);
+// 	var url = 'https://api.github.com/repos/Teradata/presto/pulls';
+// 	//getDataInput(url, res);
+// =======
+// >>>>>>> linda
+// 	req.open('GET', url, true);
+
+// 	var token = ''; // PUT YOUR PERSONAL TOKEN HERE!!!
+
+// 	req.setRequestHeader('Authorization', 'token ' + token);
+// 	// req.setRequestHeader('state', 'closed');
+// 	// req.setRequestHeader('per_page', 100);
+// 	// req.params.state = "closed";
+// 	console.log(req);
+
+
+// 	req.addEventListener('load', function(e){
+// 		if (req.status == 200) {
+// 			var data = JSON.parse(req.responseText);
+// 			response.json(data);
+// 		} else {
+// 			console.log(req.status);
+// 		}
+// 	}, false);
+
+// 	// req.send(null); 
+// 	req.send();
+// });
+
+app.get('/', function(request, response){
+	response.render('home.html');
 });
 
 // app.get('/data', function(request, response) {
