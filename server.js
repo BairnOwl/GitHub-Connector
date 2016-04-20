4d46545327641fc6d7c071ee1e283f3f719b1b27
sophieyueguo <yue@yues-mbp.devices.brown.edu>
2016-04-20 16:30:37 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 943ee49a58077f1af2fdb23528b17eb1f123f451, reversing
    changes made to 93c66f49fa4bee35eba05e3a3b5d2f8bb8f552da.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var url;
 
 var engines = require('consolidate');
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
@@ -9,19 +10,57 @@ app.use(express.static('public'));
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var req = new XMLHttpRequest();
 
+// function getDataInput(url, response) {
+// 	req.open('GET', url, true);
+
+// 	var token = 'f73a348b548c80c4675caad03698cc47fc74703f'; 	// PUT YOUR PERSONAL TOKEN HERE!!!
+
+// 	req.setRequestHeader('Authorization', 'token ' + token);
+// 	req.addEventListener('load', function(e){
+// 		if (req.status == 200) {
+// 			var data = JSON.parse(req.responseText);
+// 			response.json(data);
+// 		}
+// 	}, false);
+
+// 	req.send(null);
+// }
+
+app.post('/:inputurl', function(request, response){
+	console.log(request.params.inputurl);
+	url = 'https://api.github.com/repos/Teradata/' + request.params.inputurl + '/pulls';
+	console.log(url, response);
+	// req.open('GET', url, true);
+
+	// var token = 'f73a348b548c80c4675caad03698cc47fc74703f'; 	// PUT YOUR PERSONAL TOKEN HERE!!!
+
+	// req.setRequestHeader('Authorization', 'token ' + token);
+	// req.addEventListener('load', function(e){
+	// 	if (req.status == 200) {
+	// 		var data = JSON.parse(req.responseText);
+	// 		response.json(data);
+	// 	}
+	// }, false);
+
+	// req.send(null);
+	//console.log("server: " + url);
+});
+
 app.get('/', function(request, response){
 	response.render('home.html');
 });
 
 app.get('/data', function(request, response) {
-	var url = 'https://api.github.com/repos/prestodb/presto/pulls';
 
-	var con = req.open('GET', url, true);
+	//console.log('post' + request.params.data);
+	var url = 'https://api.github.com/repos/Teradata/presto/pulls';
+	//getDataInput(url, res);
+	req.open('GET', url, true);
 
-	var token = 'fa5fcc0d9bedde34dd3f541c70bf0547092da7cb'; 	// PUT YOUR PERSONAL TOKEN HERE!!!
+	var token = '';// PUT YOUR PERSONAL TOKEN HERE!!!
 
 	req.setRequestHeader('Authorization', 'token ' + token);
-	// req.setRequestHeader('state', 'closed');
+	//req.setRequestHeader('state', 'closed');
 	// req.setRequestHeader('per_page', 100);
 	// req.params.state = "closed";
 	console.log(req);
@@ -37,6 +76,10 @@ app.get('/data', function(request, response) {
 	// req.send(null); 
 	req.send();
 });
+
+// app.listen(process.env.PORT, function(){
+//     console.log('- Server listening on port 8080');
+// });
 
 app.listen(8080, function(){
     console.log('- Server listening on port 8080');
