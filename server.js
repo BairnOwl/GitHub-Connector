46d4fe393a115e1183a6b153db47f42736613425
lmhly <Lucy_mouse@lucydembp.devices.brown.edu>
2016-04-23 13:02:26 -0400

    wake up sleeping beauty

--- server.js
+++ server.js
@@ -90,36 +90,34 @@ app.get('/login', function(request, response) {
 app.get('/home', function(request, response) {
 	var code = request.param('code');
 	var state = request.param('state');
-	//response.render('dummy.html', {code: code, state: state});
-	var headers = request.headers;
-	// console.log('state: ' + state);
-	// console.log("code: " + code);
-	path = '/login/oauth/access_token';
-	headers.host = 'github.com';
+	response.render('dummy.html', {code: code, state: state});
+	// var headers = request.headers;
+	// path = '/login/oauth/access_token';
+	// headers.host = 'github.com';
 	
-	//var req = new XMLHttpRequest();
+	// //var req = new XMLHttpRequest();
 
-	var params = '?client_id=f112d8966964169f6ebb' + 
-				 '&client_secret=538d16b411d8a82ba90e26a298a8c40345fab874' + 
-				 '&code=' + code;
+	// var params = '?client_id=f112d8966964169f6ebb' + 
+	// 			 '&client_secret=538d16b411d8a82ba90e26a298a8c40345fab874' + 
+	// 			 '&code=' + code;
 
-	path += params;
+	// path += params;
 
-	var opts = {
-		hostname:'github.com',
-		port:'8080',
-		path: path,
-		headers: headers,
-		method: 'POST'
-	}
-	var req = https.request(opts, function(response){
-		//console.log('in http');
-		response.setEncoding('utf8');
-		res.on('data', function(data)) {
-			response.render('dummy.html', {code: code, state: state, data: data});
-		}
-		//console.log(response.param('access_toke'));
-	});
+	// var opts = {
+	// 	hostname:'github.com',
+	// 	port:'8080',
+	// 	path: path,
+	// 	headers: headers,
+	// 	method: 'POST'
+	// }
+	// var req = https.request(opts, function(response){
+	// 	//console.log('in http');
+	// 	response.setEncoding('utf8');
+	// 	res.on('data', function(data)) {
+	// 		response.render('dummy.html', {code: code, state: state, data: data});
+	// 	}
+	// 	//console.log(response.param('access_toke'));
+	// });
 	// // req.onreadystatechange = function() {
 	// // 	//console.log(req.readyState);
 	// // 	//console.log(req.status);
