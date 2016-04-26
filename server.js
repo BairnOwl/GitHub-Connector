1f16ce9e19eb3ea09901e377010c42092f0268c8
lmhly <yuchen_huang@brown.edu>
2016-04-26 15:42:17 -0400

    trying to fix status 302

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.get('/home', function(requ, response) {
     // 	});
 
 		https.get(userUrl + userToken, function(res) {
-			consolg.log('res statuscode: ' + res.statusCode);
+			console.log('res statuscode: ' + res.statusCode);
 			console.log('res headers: ' + res.headers);
 			res.headers = {'Authorization': 'User-Agent gitbuddy'};
 			res.on('data', function(userData) {
