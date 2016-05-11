2e12593c2c5f058108b728cb59c19566ea1257c3
lmhly <yuchen_huang@brown.edu>
2016-05-10 20:49:31 -0400

    cleared code

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ var bodyParser = require('body-parser');
 
 var cookieParser = require('cookie-parser');
 
-//var cookies = require('cookies');
 var rp = require('request-promise');
 
 var engines = require('consolidate');
-app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
+app.engine('html', engines.hogan);
 app.set('views', __dirname + '/templates');
 app.use(express.static('public'));
 
@@ -37,14 +36,6 @@ app.get('/cookie',function(req, res){
      res.cookie('user_name' , 'BairnOwl').send('Cookie is set');
 });
 
-//This function is credit to http://jsfiddle.net/wSQBx/
-var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-}
-
 app.post('/data/:org/:repo/:state/:per_page/:username/:page_num', function(request, response) {
 
 	var org = request.params.org;
@@ -53,9 +44,6 @@ app.post('/data/:org/:repo/:state/:per_page/:username/:page_num', function(reque
 	var per_page = request.params.per_page;
 	var username = request.params.username;
 	var page_num = request.params.page_num;
-	//var numReq = request.params.numReq;
-
-	//console.log('numReq from data.js: ' + numReq);
 
 	url = 'https://api.github.com/repos/' + org + '/' + repo + '/pulls?state=' + state + '&per_page=' + per_page + '&page=' + page_num;
 	req = new XMLHttpRequest();
@@ -87,7 +75,6 @@ app.get('/init', function(request, response){
 app.get('/login', function(request, response) {
 	var req = new XMLHttpRequest();
 
-	var random = randomString(32, chars);
 	var params = '?client_id=f112d8966964169f6ebb&state=' + 
 		random + 'scopes=user,public_repo';
 	
@@ -132,19 +119,17 @@ app.get('/home', function(requ, response) {
 		var options = {
 		    uri: 'https://api.github.com/user',
 		    qs: {
-		        access_token: userToken // -> uri + '?access_token=xxxxx%20xxxxx' 
+		        access_token: userToken 
 		    },
 		    headers: {
 		        'User-Agent': 'Request-Promise'
 		    },
-		    json: true // Automatically parses the JSON string in the response 
+		    json: true
 		};
 		 
 		rp(options)
 		    .then(function (user) {
 		    	userLogin = user.login;
-		        console.log('User login meeeeee: ' +  user.login);
-		        console.log('User token meeeeee: ' +  userToken);
 		        users[userLogin] = userToken;
 		       
 		        userToken = '';
@@ -152,7 +137,7 @@ app.get('/home', function(requ, response) {
 		        response.render('home.html', {username: userLogin, token: users[userLogin]});
 		    })
 		    .catch(function (err) {
-		        // API call failed... 
+				console.log('Get User data failed.'); 
 		    });
 	  }
 	});
